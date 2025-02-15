2645722f23c3c5be3441b0dfa8a9e8608a3b5541
Artur Komaristych <akomaristych@infolytics.com>
2025-02-15 11:24:54 +0100

    feat: add asynchronous execution for riddles

--- scripts/lmm.py
+++ scripts/lmm.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 
 import ollama
 from langchain_ollama import ChatOllama
+from langchain_openai import OpenAI
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain_core.messages import AIMessage, BaseMessage
 
@@ -22,7 +23,10 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+
+# Suppress third-party Langchain HTTP logging (https://github.com/langchain-ai/langchain/issues/14065#issuecomment-2252540350)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpcore").setLevel(logging.WARNING)
 
 
 class ChatHistory:
@@ -71,12 +75,26 @@ class LLM(ABC):
     """Interface for language model implementations"""
 
     @abstractmethod
-    def setup():
+    def load():
         """Setup the model and any required resources."""
         raise NotImplementedError
 
     @abstractmethod
     def generate(self, chat_template: ChatPromptTemplate, args: dict) -> ChatHistory:
+        """Generate text based on the provided chat template and arguments.
+
+        Args:
+            chat_template (ChatPromptTemplate): Template for formatting the prompt
+            args (dict): Arguments to fill in the template
+
+        Returns:
+            ChatHistory: Generated chat history
+        """
+        raise NotImplementedError
+
+    async def agenerate(
+        self, chat_template: ChatPromptTemplate, args: dict
+    ) -> ChatHistory:
         """Generate text based on the provided chat template and arguments.
 
         Args:
@@ -104,7 +122,57 @@ class LLM(ABC):
         raise NotImplementedError
 
 
-class OllamaLlm(LLM):
+class vLLMModel(LLM):
+    """Language model class that wraps Ollama models for text generation"""
+
+    def __init__(self, model: str):
+        """Initialize the language model with the specified model name and device
+
+        Args:
+            model (str): Name/path of the Ollama model to use
+        """
+        self.model = model
+        self.llm = OpenAI(
+            model=model,
+            base_url="http://localhost:8000/v1",
+            api_key="token-abc123",
+        )
+
+    def load(self):
+        """Ensure that the model is pulled from ollama and ready for use."""
+
+    def generate(self, chat_template: ChatPromptTemplate, args: dict) -> ChatHistory:
+        """Generate text based on the provided chat template and arguments."""
+
+        messages = chat_template.format_messages(**args)
+        response = self.llm.invoke(messages)
+        return ChatHistory([messages, response])
+
+    async def agenerate(
+        self, chat_template: ChatPromptTemplate, args: dict
+    ) -> ChatHistory:
+        """Generate text based on the provided chat template and arguments."""
+
+        messages = chat_template.format_messages(**args)
+        response = await self.llm.ainvoke(messages)
+        return ChatHistory([messages, response])
+
+    def cleanup(self):
+        """Cleanup the model by freeing up resources."""
+
+        # Delete image
+        logger.info(
+            "Ollama models will be deleted on demand and therefore this step is skipped!"
+        )
+
+    @property
+    def name(self) -> str:
+        """Get the name of the model."""
+
+        return self.model
+
+
+class OllamaModel(LLM):
     """Language model class that wraps Ollama models for text generation"""
 
     def __init__(self, model: str):
@@ -116,7 +184,7 @@ class OllamaLlm(LLM):
         self.model = model
         self.llm = ChatOllama(model=model)
 
-    def setup(self):
+    def load(self):
         """Ensure that the model is pulled from ollama and ready for use."""
 
         # Pull image
@@ -140,6 +208,15 @@ class OllamaLlm(LLM):
 
         messages = chat_template.format_messages(**args)
         response = self.llm.invoke(messages)
+        return ChatHistory([messages, response])
+
+    async def agenerate(
+        self, chat_template: ChatPromptTemplate, args: dict
+    ) -> ChatHistory:
+        """Generate text based on the provided chat template and arguments."""
+
+        messages = chat_template.format_messages(**args)
+        response = await self.llm.ainvoke(messages)
         return ChatHistory([messages, response])
 
     def cleanup(self):
